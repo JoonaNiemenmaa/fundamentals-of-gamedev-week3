30d764d127cb4d9bb876efe085a40f2b5c39cac2
Joona <joonaoskari@hotmail.com>
2025-09-09 12:15:37 +0300

    fixes to task 4

--- Scripts/meteor.gd
+++ Scripts/meteor.gd
@@ -8,11 +8,10 @@ var cur_health = MAX_HEALTH
 
 func _physics_process(delta: float) -> void:
 	position.y += MOVE_SPEED * delta
-	print(get_tree().get_current_scene() is Main)
 	if cur_health <= 0:
 		var root_node = get_tree().get_current_scene()
 		if root_node is Main:
 			root_node.ui.get_child(0).increment_score()
-			queue_free()
+		queue_free()
 	elif position.y > DESTROY_THRESHOLD:
 		queue_free()
