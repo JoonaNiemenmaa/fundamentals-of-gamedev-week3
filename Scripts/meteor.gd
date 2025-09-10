e462575e40c550d9ef0eb6b9d4cbddc88b89f107
Joona <joonaoskari@hotmail.com>
2025-09-10 09:47:29 +0300

    miksi

--- Scripts/meteor.gd
+++ Scripts/meteor.gd
@@ -6,13 +6,14 @@ const MAX_HEALTH = 3
 
 @onready var main: Main = get_tree().get_current_scene()
 var cur_health = MAX_HEALTH
+@export var ui: UI
 
 func _physics_process(delta: float) -> void:
 	position.y += MOVE_SPEED * delta
 	if cur_health <= 0:
 		print(main)
-		if main is Main:
-			main.ui.increment_score()
+		if ui:
+			ui.increment_score()
 		queue_free()
 	elif position.y > DESTROY_THRESHOLD:
 		queue_free()
@@ -20,5 +21,6 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		ui = body.ui
 		body.cur_health -= 1
 		queue_free()
